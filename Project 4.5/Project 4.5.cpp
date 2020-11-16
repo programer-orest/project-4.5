9fd50bfd4540105c12de0390904577bb1348f81d
Orest Yakymiv <akimivorest95@gmail.com>
2020-11-16 17:58:51 +0200

    first branch

---
+++ Project 4.5/Project 4.5.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    double x, y, R = 0;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "R = "; cin >> R;
+        cout << "x = "; cin >> x;
+        cout << "y = "; cin >> y;
+
+        if ((y <= (sqrt((R * R) - (x + R) * (x + R))) + R) ||
+            (y <= 0 && y >= -R && x >= 0 && x <= 2 * R))
+            cout << "yes" << endl;
+        else
+            cout << "no" << endl;
+    }
+    cout << endl << fixed;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "R = "; cin >> R;
+
+        x = -2 * R + (2 * R - (-2 * R)) * rand() / RAND_MAX;
+        y = -2 * R + (2 * R - (-2 * R)) * rand() / RAND_MAX;
+
+        if ((y <= (sqrt((R * R) - (x + R) * (x + R))) + R) ||
+            (y <= 0 && y >= -R && x >= 0 && x <= 2 * R))
+            
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "yes" << endl;
+        else
+            cout << setw(8) << setprecision(4) << x << " "
+            << setw(8) << setprecision(4) << y << " " << "no" << endl;
+    }
+
+    system("pause");
+    return 0;
+}
